62a9b3df47fc793f5da609aadf9ee5aa2b60faf8
OBI OBI ETCHU JNR <obi.junior@icloud.com>
2025-03-03 17:06:08 +0100

    first commit

---
+++ app/Http/Controllers/Authmanager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Authmanager extends Controller
+{
+    function login()
+    {
+        
+    }
+    function loginPost(Request $request){
+
+    }
+    function Register()
+    {
+        
+    }
+      
+    function registerPost(Request $request){
+        
+    }
+}   
